4c7fb93171436257d17adbef3651a48dac9bef88
Edwin <edwin008@gmail.com>
2012-04-10 05:34:53 -0500

    actualizacion

--- Calculadora/src/Punto5/LineaInvalida.java
+++
@@ -1,7 +0,0 @@
-package Punto5;
-
-public class LineaInvalida extends Exception {
-	public LineaInvalida(String linea){
-		super("L’nea no v‡lida "+linea);
-	}
-}
--- Calculadora/src/Punto7/LineaInvalida.java
+++
@@ -1,7 +0,0 @@
-package Punto7;
-
-public class LineaInvalida extends Exception {
-	public LineaInvalida(String linea){
-		super("L’nea no v‡lida "+linea);
-	}
-}
